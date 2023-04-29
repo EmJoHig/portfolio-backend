9f4bb70fbacc80da769d875b23ddb58cf0ad84e2
EmanuelHiguera <ehiguera@tesoreria.gba.gov.ar>
2023-04-29 18:58:46 -0300

    commit 20

--- src/main/java/portfolio/backend/PortfolioBackendApplication.java
+++ src/main/java/portfolio/backend/PortfolioBackendApplication.java
@@ -2,8 +2,13 @@ package portfolio.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
+
+import portfolio.backend.modelo.SecurityConfig;
+import portfolio.backend.modelo.CorsConfig;
 
 @SpringBootApplication
+@Import({SecurityConfig.class, CorsConfig.class})
 public class PortfolioBackendApplication {
 
 	public static void main(String[] args) {
